f67342e8070372cbdb883d39ce08b737be3e93bd
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2015-12-07 09:31:14 +0000

    * Made N mesh 2D instead of 3D
    * Optimized Meteor shower to pregenerate meteors than generate and destroy constantly

--- MeteorShower.js
+++ MeteorShower.js
@@ -7,6 +7,10 @@
  
 var fastRandom = goo.MathUtils.fastRandom;
 
+// ## Constants
+ 
+var MAX_METEORS = 6;
+
 // ## Global state
 
 var world   = null;
@@ -19,8 +23,7 @@ var nextSpawn = 0;
 
 function spawnNewMeteor()
 {
-    var position = new goo.Vector3();
-    var meteor   = goo.EntityUtils.clone(world, master,
+    var meteor = goo.EntityUtils.clone(world, master,
     {
         shareMeshData  : true,
         shareMaterials : true,
@@ -28,30 +31,38 @@ function spawnNewMeteor()
         shareTextures  : true
     });
     
+    meteor.setTranslation([10, 10, 10]);
+    meteor.motion = new goo.Vector3();
+    meteor.alive  = false;
+    
+    meteor.addToWorld();
+    meteors.push(meteor);
+}
+
+function respawnMeteor(meteor)
+{
+    var position = meteor.getTranslation();
+    var motion   = meteor.motion;
+    
     // X range from -1.5 to 8
     position.x  = (fastRandom() * 9.5) - 1.5;
     // Y range from 2.5 to 4
     position.y  = (fastRandom() * 1.5) + 2.5;
-    // Z can either be behind planet or in front of
+    // Z can either be behind, on horizon or in front of planet
     var zChance = fastRandom() * 90;
     
-    if      (zChance < 30)
-        position.z = -1;
-    else if (zChance < 60)
-        position.z = 0.25;
-    else
-        position.z = 4;
-    
-    meteor.setTranslation(position);
-    
+    if      (zChance < 30) position.z = -1;
+    else if (zChance < 60) position.z = 0.25;
+    else                   position.z = 4;
+        
     var scaleMag = (fastRandom() * 0.5) + 0.5;
-    meteor.setScale( new goo.Vector3(scaleMag, scaleMag, 0) );
+    meteor.setScale( [scaleMag, scaleMag, 0] );
     
     var motionMag = (fastRandom() * 0.1) + 0.1;
-    meteor.motion = new goo.Vector3(-motionMag, -motionMag, 0);
+    meteor.motion.x = -motionMag;
+    meteor.motion.y = -motionMag;
     
-    meteor.addToWorld();
-    meteors.push(meteor);
+    meteor.alive = true;
 }
 
 // ### Goo methods
@@ -63,6 +74,10 @@ var setup = function (args, ctx)
     // Hold reference to master meteor
     master = args.entity;
     master.removeFromWorld();
+    
+    // Pregenerate all meteors
+    for (var i = 0; i < MAX_METEORS; i++)
+        spawnNewMeteor();
 };
 
 var cleanup = function (args, ctx)
@@ -79,25 +94,29 @@ var update = function (args, ctx)
 {
     nextSpawn--;
     
-    if (meteors.length < 6 && nextSpawn < 0)
+    if (nextSpawn < 0)
     {
-        spawnNewMeteor();
-        // Next spawn range from 20 to 40 ticks
-        nextSpawn = (fastRandom() * 20) + 20
+        for (var i = 0; i < MAX_METEORS; i++)
+            if (!meteors[i].alive)
+            {
+                respawnMeteor(meteors[i]);
+                break;
+            }
+        
+        // Next spawn range from 10 to 20 ticks
+        nextSpawn = (fastRandom() * 10) + 10;
     }
     
-    meteors = meteors.filter(function(meteor)
+    meteors.forEach(function(meteor)
     {
-        meteor.addTranslation(meteor.motion);
+        if (!meteor.alive) return;
+
+        var pos = meteor
+            .addTranslation(meteor.motion)
+            .getTranslation();
         
-        var pos = meteor.getTranslation();
         if (pos.x < -7 || pos.y < -4)
-        {
-            meteor.removeFromWorld();
-            return false;
-        }
-        
-        return true;
+            meteor.alive = false;
     });
 };
 
